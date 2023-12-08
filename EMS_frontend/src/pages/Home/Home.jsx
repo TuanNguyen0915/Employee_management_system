4d915f4aca95131840859844f56c4655f101cb59
Tuan Nguyen <tuan.nat915@gmail.com>
2023-12-08 12:13:02 -0500

    Add user model and admin model

--- EMS_frontend/src/pages/Home/Home.jsx
+++ EMS_frontend/src/pages/Home/Home.jsx
@@ -1,39 +1,61 @@
+import { useState } from "react"
+
 const Home = () => {
+
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+    role: 'user'
+  })
+
+  const handleOnChange = e => {
+    const { name, value } = e.target
+    setUser({ ...user, [name]: value })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+  }
+
   return (
-    <div className="container flex items-center justify-center h-[100vh] overflow-hidden bg-loginBG bg-no-repeat bg-cover bg-center bg-fixed"
-    // style={{backgroundImage: `url('images/login-bg.jpg')`}}
-    >
-      <div className="md:w-1/2 md:mx-0 mx-4 w-full border border-solid border-lightGreenBG rounded-md backdrop-blur-md bg-white/10">
-        <div className="p-[40px]">
-          <h1 className="text-whiteColor text-center text-[40px] font-bold leading-[50px]">Login Page</h1>
-          <form className="flex flex-col gap-4 mt-[40px]">
-            {/* email */}
-            <label className="text-whiteColor text-[20px]">Email:</label>
-            <input
-              type="text"
-              name="email"
-              className="px-4 py-2 text-[20px] rounded-lg border border-solid border-white focus:outline-2 outline-green-600 "
-              placeholder="Enter Email"
-              autoComplete="off"
-              required
-            />
-            {/* password */}
-            <label className="text-whiteColor text-[20px]">Password:</label>
-            <input
-              type="password"
-              name="password"
-              className="px-4 py-2 text-[20px] rounded-lg border border-solid border-white focus:outline-2 outline-green-600 "
-              placeholder="Enter Email"
-              autoComplete="off"
-              required
-            />
-            <button className="btn">Login</button>
-            <div className="flex items-center justify-center">
-              <input type="checkbox"  className="h-6 w-6 bg-gray-500 rounded-full accent-green-700 focus:ring-green-400 shadow checked:shadow-xl mr-5"/>
-              <p className="text-[20px] text-white">You are Agree with <span className="text-linkText hover:text-greenText cursor-pointer">Terms & Conditions</span></p>
-            </div>
-          </form>
+    <div className="container flex items-center justify-center h-[100vh] bg-loginBG bg-cover bg-no-repeat bg-center bg-fixed overflow-hidden">
+      <div className="md:w-1/2 w-full md:mx-0 mx-4 p-4 md:p-8 backdrop-blur-md bg-white/5 flex flex-col border border-greenBorder rounded-lg">
+        <div className="w-full border-b border-white/40">
+          <h2
+            className="text-[30px] md:text-[40px] leading-[35px] md:leading-[50px] text-whiteColor text-center font-bold pb-[20px]"
+          >Login Page
+          </h2>
         </div>
+
+        <form className="flex flex-col md:gap-4 gap-3 mt-[50px]" onSubmit={handleSubmit}>
+          {/* EMAIL */}
+          <label className="text-white text-[16px] md:text-[20px]" >Email:</label>
+          <input
+            type="text" className="w-full md:py-4 md:px-6 py-2 px-4 backdrop-blur-sm bg-white/10 rounded-lg text-[16px] md:text-[20px] text-white focus:outline outline-greenBorder"
+            placeholder="Enter your email"
+            value={user.email} name="email"
+            onChange={handleOnChange}
+          />
+          {/* PASSWORD */}
+          <label className="text-white text-[16px] md:text-[20px]" >Password:</label>
+          <input
+            type="password" className="w-full md:py-4 md:px-6 py-2 px-4 backdrop-blur-sm bg-white/10 rounded-lg text-[16px] md:text-[20px] text-white focus:outline outline-greenBorder"
+            placeholder="Enter your password"
+            value={user.password} name='password'
+            onChange={handleOnChange}
+          />
+          {/* BUTTON AND TERMS */}
+          <div className="w-full flex justify-center items-center">
+            <div className="w-1/2 flex justify-center items-center">
+              <button className="w-1/2 btn px-4 py-2">Login</button>
+            </div>
+          </div>
+          <div className="w-full flex items-center justify-center">
+            <input type="checkbox" className="w-8 h-8 rounded-full shadow checked:shadow-xl accent-lightGreenBG mr-6" />
+            <p className="text-[20px] text-white">You are Agree with <span className="text-linkText hover:text-greenText cursor-pointer">Terms & Conditions</span></p>
+          </div>
+        </form>
       </div>
     </div>
   )
