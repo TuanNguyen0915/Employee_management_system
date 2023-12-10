b9e5b4f59dd3c42b75d984733f09fca69a584a74
Tuan Nguyen <tuan.nat915@gmail.com>
2023-12-10 13:38:53 -0500

    add mobile toggle for navbar

--- EMS_frontend/src/components/NavBar/NavBar.jsx
+++ EMS_frontend/src/components/NavBar/NavBar.jsx
@@ -3,43 +3,65 @@ import { navList } from "../../utils/data/data"
 import { Link } from "react-router-dom"
 import { AiOutlineDashboard } from "react-icons/ai";
 import { FaPeopleRoof } from "react-icons/fa6";
-import { BiCategory } from "react-icons/bi";
+import { BiCategory, BiMenu } from "react-icons/bi";
 import { ImProfile } from "react-icons/im";
 import { LuLogOut } from "react-icons/lu";
+import { GrClose } from "react-icons/gr";
+import { useState } from "react";
 
 
 const NavBar = ({ tab, setTab }) => {
-
+  const [toggle, setToggle] = useState(false)
   return (
-    <>
-      <h2 className="w-full mt-[4rem] mb-[10rem] text-center text-lightGreenBG font-bold text-[20px] md:text-[24px]">Employees Management</h2>
-      <ul className=" pl-[10px] md:pl-[40px]">
-        {navList.map((link, idx) => (
-          <li
-            key={idx}
-            className={`${idx < navList.length - 1 ? 'mb-[40px]' : 'mb-0'}`}
-          >
-            <Link
-              onClick={() => setTab(link.name)}
-              to={link.to}
-              className={`${tab === link.name ? 'text-greenText pl-[20px]' : 'text-white'} flex gap-2 md:gap-4 text-[16px] md:text-[20px] font-semibold`}
+    <div
+      className="md:w-1/4">
+      <div
+        className="md:hidden object-contain"
+        onClick={() => setToggle(!toggle)}
+      >
+        {toggle
+          ? <GrClose className="text-[40px] text-lightGreenBG cursor-pointer" />
+          : <BiMenu className="text-[40px] text-lightGreenBG cursor-pointer" />
+        }
+      </div>
+      <div
+        className={`${toggle ? 'block' : 'hidden '} md:block`}
+      >
+        <h2
+          className="w-full mt-[4rem] mb-[5rem] md:mb-[10rem] text-center text-lightGreenBG font-bold text-[20px] md:text-[24px]"
+        >
+          Hi, user&#39;s name here
+        </h2>
+        <ul className="pl-0 md:pl-[40px]">
+          {navList.map((link, idx) => (
+            <li
+              key={idx}
+              className={`${idx < navList.length - 1 ? 'mb-[20px] md:mb-[40px]' : 'mb-0'} w-full px-[10px]`}
             >
-
-              <div className="w-10 h-10 flex justify-center items-center">
-                {link.name === 'DashBoard' && <AiOutlineDashboard className="w-10 h-10" />}
-                {link.name === 'Manage Employees' && <FaPeopleRoof className="w-10 h-10" />}
-                {link.name === 'Category' && <BiCategory className="w-10 h-10" />}
-                {link.name === 'Profile' && <ImProfile className="w-10 h-10" />}
-                {link.name === 'LogOut' && <LuLogOut className="w-10 h-10" />}
-              </div>
-              <p className="translate-y-1">
-                {link.name}
-              </p>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </>
+              <Link
+                onClick={() => setTab(link.name)}
+                to={link.to}
+                className={`${tab === link.name ? 'text-greenText md:pl-[40px] backdrop-blur-md bg-white/10 rounded-lg' : 'text-white'} 
+              flex gap-2 md:gap-4 text-[14px] md:text-[20px] font-semibold px-4 py-2
+              hover:backdrop-blur-md hover:bg-white/40  hover:rounded-lg
+              `}
+              >
+                <div className="w-10 h-10 flex justify-center items-center">
+                  {link.name === 'DashBoard' && <AiOutlineDashboard className="md:w-10 md:h-10 w-7 h-7 " />}
+                  {link.name === 'Manage Employees' && <FaPeopleRoof className="md:w-10 md:h-10 w-7 h-7 translate-y-2" />}
+                  {link.name === 'Category' && <BiCategory className="md:w-10 md:h-10 w-7 h-7 " />}
+                  {link.name === 'Profile' && <ImProfile className="md:w-10 md:h-10 w-7 h-7 " />}
+                  {link.name === 'LogOut' && <LuLogOut className="md:w-10 md:h-10 w-7 h-7 " />}
+                </div>
+                <p className="md:translate-y-1 translate-y-3">
+                  {link.name}
+                </p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div >
+    </div>
   )
 }
 
--- EMS_frontend/src/pages/DashBoard/DashBoard.jsx
+++ EMS_frontend/src/pages/DashBoard/DashBoard.jsx
@@ -2,15 +2,15 @@ import { useState } from "react"
 import NavBar from "../../components/NavBar/NavBar"
 
 const DashBoard = () => {
-  const [tab,setTab] = useState('DashBoard')
+  const [tab, setTab] = useState('DashBoard')
+  
   return (
     <section>
-      <div className="container flex">
-        <div className="md:w-1/4 w-1/2 h-[100vh] flex flex-col items-start bg-secondColor ">
+      <div className="container flex w-full">
           <NavBar tab={tab} setTab={setTab}/>
-        </div>
+
         {/* CONTENT */}
-        <div className="w-3/4 flex flex-col bg-slate-300">
+        <div className="md:w-3/4 w-full h-[100vh] flex flex-col bg-slate-300">
           DASHBOARD CONTENT
         </div>
       </div>
