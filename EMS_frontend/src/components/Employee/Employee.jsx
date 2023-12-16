f16146d8156877d79fad04ef6adaa05ea06be9de
Tuan Nguyen <tuan.nat915@gmail.com>
2023-12-15 20:35:19 -0500

    Add employees list

--- EMS_frontend/src/components/Employee/Employee.jsx
+++ EMS_frontend/src/components/Employee/Employee.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { toast } from "react-toastify"
+import { RxAvatar } from "react-icons/rx"
 
 const Employee = () => {
   const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/employees`
@@ -25,20 +26,35 @@ const Employee = () => {
         <table className="table-auto">
           <thead className="border-b-2 border-slate-400">
             <tr>
+              <th className="pr-2 md:pr-4 font-semibold tracking-wide text-center hidden md:block"></th>
               <th className="pr-2 md:pr-4 font-semibold tracking-wide text-center ">Name</th>
               <th className="pr-2 md:pr-4 font-semibold tracking-wide text-center ">Email</th>
               <th className="pr-2 md:pr-4 font-semibold tracking-wide text-center ">Category</th>
+              <th className="pr-2 md:pr-4 font-semibold tracking-wide text-center "></th>
             </tr>
           </thead>
           <tbody>
-            {employees.map((e,idx) => (
-              <tr 
-              key={e._id}
-              className={`${idx !== 0?'bg-slate-400':'bg-transparent'} py-2`}
-              >
-                <td className="p-2 md:p-4">{e.firstName} {e.lastName}</td>
+            {employees.map((e, idx) => (
+              <tr key={e._id} className={`${idx !== 0 ? "bg-slate-400" : "bg-transparent"}`}>
+                <div className="w-full h-full hidden md:flex justify-center items-center ">
+                  <td className="p-2 md:p-4">
+                    {e.photo ? (
+                      <img src={e.photo} alt="" className="w-full rounded-full" />
+                    ) : (
+                      <RxAvatar style={{ scale: "2" }} />
+                    )}
+                  </td>
+                </div>
+                <td className="p-2 md:p-4">
+                  {e.firstName} {e.lastName}
+                </td>
                 <td className="p-2 md:p-4">{e.email}</td>
                 <td className="p-2 md:p-4">{e.category}</td>
+                <td className="p-2 md:p-4">
+                  <Link to={`/employees/${e._id}`}>
+                    <button className="btn border-none p-1 text-[16px]">Details</button>
+                  </Link>
+                </td>
               </tr>
             ))}
           </tbody>
